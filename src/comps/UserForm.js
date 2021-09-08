de5280c5086cae5adfd4d9a65046ca16770da20e
BangayaDiall <bangayadiall@gmail.com>
2021-09-08 12:19:36 +0100

    added form to user form

--- src/comps/UserForm.js
+++ src/comps/UserForm.js
@@ -1,10 +1,40 @@
 import React from 'react';
 
 class UserForm extends React.Component {
+   constructor(){
+     super();
+     // we create our state
+     this.state = {
+       name: "",
+       email: "",
+       gender: ""
+     };
+
+   }
+   // to update our state
+   handleName(e){
+     this.setState({name : e.target.value})
+   };
+   handleEmail(e){
+     this.setState({email: e.target.value})
+   };
+   handleGender(e){
+     this.setState({gender: e.target.value})
+   };
+   handleSubmit(e){
+     e.preventDefault()
+     this.setState({name: "",email: "",gender: ""})
+   };
+
   render(){
     return (
       <div>
-       <h1>hi I am a create user!</h1>
+       <form onSubmit = {(e) => this.handleSubmit(e)}>
+           <input name = 'name' type='txt' value = {this.state.name} onChange= {(e) => this.handleName(e)} />
+           <input name = 'email' type='email' value = {this.state.email} onChange= {(e)=> this.handleEmail(e)} />
+           <input name = 'gender' type = 'txt' value={this.state.gender} onChange = {(e) => this.handleGender(e)} />
+           <input type = "submit" />
+       </form>
       </div>
     );
   }
