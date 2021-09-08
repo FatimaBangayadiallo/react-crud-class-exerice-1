0511b61359167c63c01509667cd58673aaed35c9
BangayaDiall <bangayadiall@gmail.com>
2021-09-08 11:56:53 +0100

    create Userform and UserList class components

---
+++ src/comps/UserForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class UserForm extends React.Component {
+  render(){
+    return (
+      <div>
+       <h1>hi I am a create user!</h1>
+      </div>
+    );
+  }
+};
+export default UserForm;
